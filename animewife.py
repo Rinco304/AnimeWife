456f5533364d14cd8618030a0413cb27023c340b
Rlezzo <86238734+Rlezzo@users.noreply.github.com>
2024-05-14 17:08:24 +0800

    Update animewife.py
    修复了添加老婆图片下载失败的bug，更改了牛老婆的概率机制

--- animewife.py
+++ animewife.py
@@ -4,6 +4,7 @@ from hoshino.config import RES_DIR
 from hoshino.typing import CQEvent
 from hoshino.util import DailyNumberLimiter
 import asyncio
+from html import unescape
 
 # 加载json数据
 def load_group_config(group_id: str) -> int:
@@ -33,20 +34,22 @@ mlmt= DailyNumberLimiter(_max)
 # 当超出次数时的提示
 max_notice = f'为防止滥用，管理员一天最多可添加{_max}次，若需添加更多请使用 来杯咖啡 联系维护组'
 
-# 每人每天可成功牛到老婆的次数
-_ntr_max=2
+# 每人每天可牛老婆的次数
+_ntr_max=10
 ntr_lmt= DailyNumberLimiter(_ntr_max)
 # 当超出次数时的提示
-ntr_max_notice = f'一天最多牛{_ntr_max}次，你不许再牛了！'
+ntr_max_notice = f'为防止牛头人泛滥，一天最多可牛{_ntr_max}次（无保底），若需添加更多请使用 来杯咖啡 联系维护组'
+# 牛老婆的成功率
+ntr_possibility = 0.25
 
 sv_help = '''
 [抽老婆] 看看今天的二次元老婆是谁
 [添加老婆+人物名称+图片] 群管理员每天可以添加一次人物
-[交换老婆] @某人 + 交换老婆 申请和群友交换老婆
+※为防止bot被封号和数据污染请勿上传太涩与功能无关的图片※
+[交换老婆] @某人 + 交换老婆
 [牛老婆] 25%概率牛到别人老婆(2次/日)
 [查老婆] 加@某人可以查别人老婆
-[切换ntr开关状态] 群管理员可以 打开/关闭 本群ntr功能
-※为防止bot被封号和数据污染请勿上传太涩或与功能无关的图片※
+[切换ntr开关状态]
 '''.strip()
 
 sv = Service(
@@ -58,6 +61,7 @@ sv = Service(
     bundle = '娱乐', #分组归类
     help_ = sv_help #帮助说明
     )
+
 
 @sv.on_fullmatch('抽老婆')
 async def animewife(bot, ev: CQEvent):
@@ -107,6 +111,7 @@ async def animewife(bot, ev: CQEvent):
     
 #下载图片
 async def download_async(url: str, name: str):
+    url = unescape(url)
     resp= await aiorequests.get(url, stream=True)
     if resp.status_code == 404:
         raise ValueError('文件不存在')
@@ -154,13 +159,13 @@ async def add_wife(bot,ev:CQEvent):
     await bot.send(ev,'信息已增加~')
 
 #################################### 下面是交换老婆功能 #######################################
-import asyncio
 
 class ExchangeManager:
     def __init__(self):
         self.exchange_requests = {}
         self.exchange_in_progress = {}  # 为每个群组添加交换进行标志
-
+        self.exchange_tasks = {}
+        
     def insert_exchange_request(self, group_id, user_id, target_id):
         group_id_str = str(group_id)
         user_pair = f"{user_id}-{target_id}"
@@ -195,13 +200,15 @@ class ExchangeManager:
         return user_pair in group_exchanges and group_exchanges[user_pair] == "pending"
         
     async def handle_timeout(self, bot, ev, group_id, user_id, target_id, delay = 60):
-        await asyncio.sleep(delay)  # 默认等待60秒
-        # 检查请求是否仍然是pending状态，如果是，则移除
-        if self.is_exchange_pending(group_id, user_id, target_id):
-            self.remove_exchange_request(group_id, user_id, target_id)
-            # 发送交换超时的通知
-            await bot.send_group_msg(group_id=group_id, message=f"[CQ:at,qq={user_id}] 你的交换请求已超时，对方无视了你")
-            
+        try:
+            await asyncio.sleep(delay)  # 默认等待60秒
+            # 检查请求是否仍然是pending状态，如果是，则移除
+            if self.is_exchange_pending(group_id, user_id, target_id):
+                self.remove_exchange_request(group_id, user_id, target_id)
+                # 发送交换超时的通知
+                await bot.send_group_msg(group_id=group_id, message=f"[CQ:at,qq={user_id}] 你的交换请求已超时，对方无视了你")
+        except asyncio.CancelledError:
+            pass # 如果任务取消，什么也不做或者进行其他清理工作 
     def get_exchange_by_target(self, group_id, target_id):
         group_id_str = str(group_id)
         target_id_str = str(target_id)
@@ -210,7 +217,20 @@ class ExchangeManager:
             if key.endswith(f"-{target_id_str}") and status == "pending":
                 return key.split('-')[0], key.split('-')[1]  # 返回发起者和目标者的ID
         return None, None
-
+    # 新增加两个方法来处理定时器任务的存储和取消
+    def set_exchange_task(self, group_id, user_id, target_id, task):
+        group_id_str = str(group_id)
+        user_pair = f"{user_id}-{target_id}"
+        self.exchange_tasks[group_id_str] = self.exchange_tasks.get(group_id_str, {})
+        self.exchange_tasks[group_id_str][user_pair] = task
+    
+    def cancel_exchange_task(self, group_id, user_id, target_id):
+        group_id_str = str(group_id)
+        user_pair = f"{user_id}-{target_id}"
+        task = self.exchange_tasks.get(group_id_str, {}).pop(user_pair, None)
+        if task:
+            task.cancel()
+            
 exchange_manager = ExchangeManager()
 
 @sv.on_prefix('交换老婆')
@@ -227,8 +247,10 @@ async def exchange_wife(bot, ev: CQEvent):
     for seg in ev.message:
         if seg.type == 'at' and seg.data['qq'] != 'all':
             target_id = int(seg.data['qq'])
+            #print("提取目标用户的QQ号：" + str(target_id))
             break
     if not target_id:
+        #print("未找到目标用户QQ或者未@对方")
         await bot.send(ev, '请指定一个要交换老婆的目标', at_sender=True)
         return
     # 检查发起者或目标者是否已经在任何交换中
@@ -241,14 +263,14 @@ async def exchange_wife(bot, ev: CQEvent):
         return
     # 检查是否尝试交换给自己
     if user_id == target_id:
-        await bot.send(ev, '不能和自己交换老婆哦~', at_sender=True)
+        await bot.send(ev, '不能牛自己', at_sender=True)
         return
     if not config:
-        await bot.send(ev, '没有找到本群老婆信息', at_sender=True)
+        await bot.send(ev, '没有找到本群婚姻登记信息', at_sender=True)
         return
     # 检查用户和目标用户是否有老婆信息
     if str(user_id) not in config or str(target_id) not in config:
-        await bot.send(ev, '双方都有老婆才能交换哦~', at_sender=True)
+        await bot.send(ev, '需要双方都有老婆才能交换', at_sender=True)
         return
     # 检查用户的老婆信息是否是今天
     if config[str(user_id)][1] != today:
@@ -261,9 +283,11 @@ async def exchange_wife(bot, ev: CQEvent):
     # 满足交换条件，添加进交换请求列表中
     exchange_manager.insert_exchange_request(group_id, user_id, target_id)
     # 发送交换请求
-    await bot.send(ev, f'[CQ:at,qq={target_id}] 群友 [CQ:at,qq={user_id}] 想要和你交换老婆，是否同意？\n(60秒内发送同意(拒绝))', at_sender=False)
+    await bot.send(ev, f'[CQ:at,qq={target_id}] 用户 [CQ:at,qq={user_id}] 想要和你交换老婆，是否同意？\n如果同意(拒绝)请在60秒内发送“同意(拒绝)”', at_sender=False)
     # 启动定时器，60秒后如果没有收到回应则自动清除交换请求
-    asyncio.create_task(exchange_manager.handle_timeout(bot, ev, group_id, user_id, target_id))
+    exchange_task = asyncio.create_task(exchange_manager.handle_timeout(bot, ev, group_id, user_id, target_id))
+    exchange_manager.set_exchange_task(group_id, user_id, target_id, exchange_task)
+
 
 async def handle_ex_wife(user_id, target_id, group_id, agree = False):
     if agree:
@@ -271,6 +295,7 @@ async def handle_ex_wife(user_id, target_id, group_id, agree = False):
         # 检索用户和目标用户的老婆信息
         user_wife = config.get(str(user_id), [None])[0]
         target_wife = config.get(str(target_id), [None])[0]
+        #print("发起用户老婆名称：" + str(user_wife) + "目标对象老婆名称：" + str(target_wife))
         # 交换图片名
         config[str(user_id)][0], config[str(target_id)][0] = target_wife, user_wife
         
@@ -280,6 +305,8 @@ async def handle_ex_wife(user_id, target_id, group_id, agree = False):
         write_group_config(str(group_id), str(target_id), user_wife, today, config)
     # 删除exchange_manager中对应的请求用户对记录
     exchange_manager.remove_exchange_request(group_id, user_id, target_id)
+    # 取消超时任务
+    exchange_manager.cancel_exchange_task(group_id, user_id, target_id)
     
 @sv.on_message('group')
 async def ex_wife_reply(bot, ev: CQEvent):
@@ -289,18 +316,36 @@ async def ex_wife_reply(bot, ev: CQEvent):
     # 存在交换请求
     group_id = ev.group_id
     target_id = ev.user_id
+    #print("被请求者:" + str(target_id))
     # 比对该用户是否是用户对中的被请求者
     # 通过被请求者获取发起者id
     initiator_id = exchange_manager.get_exchange_by_target(group_id, target_id)[0]
     # 不为空则说明有记录
     if initiator_id:
-        # 是被请求者，查看文本中是否包含关键词
+        # 提取消息文本
         keyword = "".join(seg.data['text'].strip() for seg in ev.message if seg.type == 'text')
-        # 检查关键词中是否包含“同意”，如果检测到了同意就执行交换
-        if '拒绝' in keyword or '不同意'in keyword:
+
+        # 寻找关键词的索引位置
+        agree_index = keyword.find('同意')
+        disagree_index = keyword.find('不同意')
+        refuse_index = keyword.find('拒绝')
+
+        # 对“不同意”和“拒绝”做处理，找出两者中首次出现的位置
+        disagree_first_index = -1
+        if disagree_index != -1 and refuse_index != -1:
+            disagree_first_index = min(disagree_index, refuse_index)
+        elif disagree_index != -1:
+            disagree_first_index = disagree_index
+        elif refuse_index != -1:
+            disagree_first_index = refuse_index
+        
+        # 进行判断
+        if disagree_first_index != -1 and (agree_index > disagree_first_index or agree_index == -1):
+            # 如果找到“不同意”或“拒绝”，且它们在“同意”之前出现，或者没找到“同意”
             await handle_ex_wife(initiator_id, target_id, group_id)
             await bot.send(ev, '对方拒绝了你的交换请求', at_sender=True)
-        elif '同意' in keyword:
+        elif agree_index != -1 and (disagree_first_index > agree_index or disagree_first_index == -1):
+            # 如果仅找到“同意”，或者“同意”在“不同意”或“拒绝”之前出现
             await handle_ex_wife(initiator_id, target_id, group_id, True)
             await bot.send(ev, '交换成功', at_sender=True)
             
@@ -326,9 +371,11 @@ async def ntr_wife(bot, ev: CQEvent):
     for seg in ev.message:
         if seg.type == 'at' and seg.data['qq'] != 'all':
             target_id = int(seg.data['qq'])
+            #print("提取目标用户的QQ号：" + str(target_id))
             break
     if not target_id:
-        await bot.send(ev, '请选择一个要下手的目标', at_sender=True)
+        #print("未找到目标用户QQ或者未@对方")
+        await bot.send(ev, '请指定一个要下手的目标', at_sender=True)
         return
     # 检查发起者或目标者是否已经在任何交换中
     if not exchange_manager.is_eligible_for_exchange(group_id, user_id, target_id):
@@ -340,14 +387,14 @@ async def ntr_wife(bot, ev: CQEvent):
         return
     # 检查是否尝试交换给自己
     if user_id == target_id:
-        await bot.send(ev, '不能牛自己！', at_sender=True)
+        await bot.send(ev, '不能牛自己', at_sender=True)
         return
     if not config:
-        await bot.send(ev, '没有找到本群老婆信息', at_sender=True)
+        await bot.send(ev, '没有找到本群婚姻登记信息', at_sender=True)
         return
     # 检查目标用户是否有老婆信息
     if str(target_id) not in config:
-        await bot.send(ev, '对方还没有老婆呢，换个人试试吧', at_sender=True)
+        await bot.send(ev, '需要对方有老婆才能牛', at_sender=True)
         return
     # 检查目标的老婆信息是否是今天
     if config[str(target_id)][1] != today:
@@ -355,9 +402,10 @@ async def ntr_wife(bot, ev: CQEvent):
         return
     # 满足交换条件，添加进交换请求列表中
     exchange_manager.insert_exchange_request(group_id, user_id, target_id)
-    ntr_lmt.increase(user_id)
-    if random.random() < 0.25: 
-        # 出货率
+    # 牛老婆次数减少一次
+    if user_id not in hoshino.config.SUPERUSERS:
+        ntr_lmt.increase(user_id)
+    if random.random() < ntr_possibility: 
         # 删除双方老婆信息，将他人老婆信息改成自己的
         target_wife = config.get(str(target_id), [None])[0]
         del config[str(target_id)]
@@ -365,7 +413,7 @@ async def ntr_wife(bot, ev: CQEvent):
         write_group_config(str(group_id), str(user_id), target_wife, today, config)
         await bot.send(ev, '你的阴谋已成功！', at_sender=True)
     else:
-        await bot.send(ev, '你的阴谋失败了，黄毛被干掉了！', at_sender=True)
+        await bot.send(ev, f'你的阴谋失败了，黄毛被干掉了！你还有{_ntr_max - ntr_lmt.get_num(user_id) - 1}条命', at_sender=True)
     # 清除交换请求锁
     exchange_manager.remove_exchange_request(group_id, user_id, target_id)
     await asyncio.sleep(1)
@@ -438,7 +486,7 @@ async def search_wife(bot, ev: CQEvent):
     # 获取用户和目标用户的配置信息
     config = load_group_config(group_id)
     if config is not None:
-        if str(target_id) in config:  
+        if str(target_id) in config:  # Making sure we're comparing strings with strings, or integers with integers
             if config[str(target_id)][1] == today:
                 wife_name = config[str(target_id)][0]
             else:
